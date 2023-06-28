60f72d11318a28c8e6a715ca15a859fb2f349996
Ric Charles Paquibot <ric2thiss@gmail.com>
2023-06-29 01:28:36 +0800

    revision

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ let cursor2 = document.querySelector('.cursor2');
 document.addEventListener('mousemove', function(e) {
     cursor1.style.cssText = cursor2.style.cssText = "left:" + e.clientX + 'px; top:' + e.clientY + 'px;';
 });
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
